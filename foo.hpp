f614ed833bf16ef79be7eec56af87b6624bb9d93
RZawila <ryszaw46@gmail.com>
2022-12-13 09:20:33 +0100

    Versja 1

--- foo.hpp
+++ foo.hpp
@@ -2,15 +2,62 @@
 
 #include "Human.hpp"
 
-#include <algorithm>
 #include <list>
 #include <vector>
+#include <algorithm>
+#include <numeric>
+
+void birth(iterator it){it->birthday();};
+
+char monster(iterator it){
+    if(it->isMonster())
+    {
+        return 'n';
+    }
+    else
+    {
+        return 'y';
+    }
+}
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > ret_v(people.size());
-
     // Twoja implementacja tutaj
+       std::vector< char > Goo(people.size(),'n');
+       //int  i = people.size()-1;//bo liczymy od zera.
+       // inkrementacja age
+       std::for_each(people.begin(),people.end(),birth);
+       //sprawdzenie is monster
+       std::transform (people.begin(), people.end(), Goo.begin(),monster);
+       std::reverse(Goo.begin(), Goo.end());
 
-    return ret_v;
+
+
+
+
+
+
+
+
+
+
+       /*
+       for(auto it = people.begin();it != people.end();it++)
+       {
+           it->birthday();
+        if(i>=0)
+        {
+           if(it->isMonster())
+           {
+               Goo[i] = 'n';
+           }
+           else
+           {
+               Goo[i] = 'y';
+           }
+           i--;
+        }
+       }
+*/
+    return Goo;
 }
