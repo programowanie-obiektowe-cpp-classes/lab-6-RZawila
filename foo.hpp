a15714ea759e124e5e081ecb809820bce93c4ba5
RZawila <ryszaw46@gmail.com>
2022-12-18 12:46:41 +0100

    juz mam tego dosc

--- foo.hpp
+++ foo.hpp
@@ -12,9 +12,9 @@ std::vector< char > foo(std::list< Human >& people)
 
        std::vector< char > Goo(people.size(),'n');
 
-       std::for_each(people.begin(),people.end(),[](Human* h){h->birthday();});
+       std::for_each(people.begin(),people.end(),[](Human& h){h.birthday();});
 
-       std::transform (people.begin(), people.end(), Goo.begin(),[](Human* h){if(h->isMonster()){return 'n';}else{return 'y';}});
+       std::transform (people.begin(), people.end(), Goo.begin(),[](Human& h){if(h.isMonster()){return 'n';}else{return 'y';}});
 
        std::reverse(Goo.begin(), Goo.end());
 
